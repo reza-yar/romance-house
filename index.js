a75f0fbd07c3ff7600a4e9eeb05711581da48dc5
reza yarahmadi <rezayarjob@gmail.com>
2023-12-08 08:24:46 +0330

    title animation develope

---
+++ index.js
@@ -0,0 +1,13 @@
+let onload_page = document.getElementById('onload_page');
+let titel_h1 = document.getElementById('title_h1');
+let titel_h2 = document.getElementById('title_h2');
+let site_container = document.querySelector('.site_container');
+
+window.addEventListener('load', () => {
+    setTimeout(function () {
+        onload_page.style.display = 'none';
+        site_container.style.overflowY = 'auto';
+        titel_h1.style.animation = 'title_anime 2s';
+        titel_h2.style.animation = 'title_anime_2 2s';
+    }, 3000)
+})
