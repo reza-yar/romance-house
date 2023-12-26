764e0270712b95f5401c8917fe85ec633ca278ce
reza yarahmadi <rezayarjob@gmail.com>
2023-12-26 18:59:00 +0330

    nav develop

--- scripts/fetch.js
+++
@@ -1,7 +0,0 @@
-
-fetch('https://romancehouse-e1018-default-rtdb.firebaseio.com/sofa.json')
-        .then( res => res.json() )
-        .then( data => {
-            console.log(Object.entries(data).length);
-            
-        })
